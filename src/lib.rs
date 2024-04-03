e04b977ae63fbbbc3d6e6a7d4343d775e63cf381
David O <148862921+David001d@users.noreply.github.com>
2024-04-03 02:08:25 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -7,5 +7,19 @@ pub enum Comparison {
 }
 
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    unimplemented!("Determine if the first list is equal to, sublist of, superlist of or unequal to the second list.");
+    if _first_list == _second_list {
+        return Comparison::Equal;
+    }
+
+    if _first_list.len() > _second_list.len() {
+        if _first_list.windows(_second_list.len()).any(|w| w == _second_list) {
+            return Comparison::Superlist;
+        }
+    } else if _first_list.len() < _second_list.len() {
+        if _second_list.windows(_first_list.len()).any(|w| w == _first_list) {
+            return Comparison::Sublist;
+        }
+    }
+
+    Comparison::Unequal
 }
